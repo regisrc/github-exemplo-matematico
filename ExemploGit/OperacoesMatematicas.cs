2111dfb7e51fb992e72d3d3f50a4d2ff92103100
FILIPE DE MELLO CUNHA <filipe.cunha@alunos.sc.senac.br>
2022-10-07 20:08:02 -0300

    filipeMultiplicar

--- ExemploGit/OperacoesMatematicas.cs
+++ ExemploGit/OperacoesMatematicas.cs
@@ -14,7 +14,13 @@
 
         public void Multiplicar()
         {
+            Console.WriteLine("Digite o 1º número: ");
+            double n1 = Convert.ToDouble(Console.ReadLine());
 
+            Console.WriteLine("Digite o 2º número: ");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"O resultado da multiplicação é {n1 * n2}");
         }
 
         public void Dividir()
