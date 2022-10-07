c105b519c2673fb005be20d67cf27d4c6b7e971b
FILIPE EDUARDO REGIS <filipe.regis@prof.sc.senac.br>
2022-10-07 19:43:44 -0300

    Criacao base projeto

---
+++ ExemploGit/OperacoesMatematicas.cs
@@ -0,0 +1,25 @@
+﻿namespace ExemploGit
+{
+    public class OperacoesMatematicas
+    {
+        public void Somar()
+        {
+
+        }
+
+        public void Subtrair()
+        {
+
+        }
+
+        public void Multiplicar()
+        {
+
+        }
+
+        public void Dividir()
+        {
+
+        }
+    }
+}
---
+++ ExemploGit/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ExemploGit;
+
+var menu = true;
+var operacoesMatematicas = new OperacoesMatematicas();
+
+while(menu)
+{
+    Console.WriteLine("Digite a opção: ");
+    var opcao = int.Parse(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 1:
+            operacoesMatematicas.Somar();
+            break;
+        case 2:
+            operacoesMatematicas.Subtrair();
+            break;
+        case 3:
+            operacoesMatematicas.Multiplicar();
+            break;
+        case 4:
+            operacoesMatematicas.Dividir();
+            break;
+        case 5:
+            Console.WriteLine("Adeus!!");
+            menu = false;
+            break;
+        default:
+            Console.WriteLine("Opção não cadastrada!");
+            break;
+    }
+}
